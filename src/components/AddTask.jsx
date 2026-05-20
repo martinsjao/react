667267fa2272f59f5c3e716c737845278d9596ac
João Martins <133363891+martinsjao@users.noreply.github.com>
2026-05-20 07:29:24 -0300

    Implement task addition and deletion functionality with UI updates

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,25 @@
-function AddTask() {
-  return <h1>Add Task</h1>;
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o titulo da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded md"
+        value={title}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded md"
+        value={description}
+      />
+      <button className="bg-slate-400 text-white px-4 py-2 rounded-md">
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,24 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(task.id)}
+            onClick={() => onTaskClick(task.id)}
             className={`bg-slate-400 text-white w-full p-2 rounded-md text-left ${task.isCompleted && "line-through"}`}
           >
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
